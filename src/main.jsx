a6cb95e047689a01268f416fa917ad80a2405034
Abdulrazak10-ghost <Itzrixn@gmail.com>
2025-09-20 22:25:43 +0100

    Updated favicon to use logo.png

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import logo from "./assets/logo.png";
+
+const link = document.querySelector("link[rel~='icon']");
+if (link) {
+  link.href = logo;
+}
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
